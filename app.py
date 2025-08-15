b0a153390353b69aa20e39f7c43b911a078c89d7
saurav1729 <i.m.saurav003@gmail.com>
2025-08-16 00:55:50 +0530

    fix app.py

--- app.py
+++ app.py
@@ -134,5 +134,9 @@ def get_historical_data(ticker):
         return jsonify({"error": f"Error fetching historical data: {str(e)}"}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
